5610fe6144ca2d708af18efc2357b57ca5a45b97
zhixinli0904 <30875204+zhixinli0904@users.noreply.github.com>
2017-08-14 08:55:24 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello(){
+  // created function to say hello//
+  var userName= prompt("what is your name?");
+  // created a variable called userName and ask the user for variable of username//
+  window.alert("hello "+ userName);
+  // created a popup window telling the userName hello//
+}
